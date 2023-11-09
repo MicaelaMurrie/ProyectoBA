3300281b742eb76885fef7b0039d6b33b83cbaa0
Micaela Murrie <micaelamurrie@gmail.com>
2023-11-08 21:50:09 -0300

    validación phone, audio

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,5 +1,6 @@
 const nombre = document.getElementById("name");
 const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 const form = document.getElementById("form");
 const parrafo = document.getElementById("warnings");
 
@@ -8,14 +9,20 @@ form.addEventListener("submit", (e) => {
   let warnings = "";
   let entrar = false;
   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let regexPhone = /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*/;
   parrafo.innerHTML = "";
   if (nombre.value.length < 3) {
-    warnings += `El nombre no es valido <br>`;
+    warnings += `El nombre no es válido <br>`;
     entrar = true;
   }
 
   if (!regexEmail.test(email.value)) {
-    warnings += `El email no es valido <br>`;
+    warnings += `El email no es válido <br>`;
+    entrar = true;
+  }
+
+  if (!regexPhone.test(phone.value)) {
+    warnings += `El teléfono no es válido <br>`;
     entrar = true;
   }
 
