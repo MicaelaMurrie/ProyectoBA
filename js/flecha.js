387d8dfcf0c04a7803f94948c7d6d99c75791f71
Micaela Murrie <micaelamurrie@gmail.com>
2023-11-10 17:22:04 -0300

    Añadí flecha para subir

---
+++ js/flecha.js
@@ -0,0 +1,15 @@
+const flecha = document.querySelector(".flecha");
+
+window.addEventListener("scroll", () => {
+  if (scrollY >= 100 && scrollY <= 1900) {
+    flecha.style.visibility = "visible";
+    flecha.style.opacity = "1";
+  } else {
+    flecha.style.visibility = "hidden";
+    flecha.style.opacity = "0";
+  }
+
+  flecha.addEventListener("click", () => {
+    scrollTo(0, 0);
+  });
+});
